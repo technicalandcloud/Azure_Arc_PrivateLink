176a3be999e3062e8c062b082722a635f1d1f4eb
Technical And Cloud <82224047+technicalandcloud@users.noreply.github.com>
2025-05-21 16:03:02 +0200

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,33 +1,32 @@
 variable "client_id" {
-  description = "Veuillez insérer votre client ID (AppId)"
+  description = "Please enter your Client ID (AppId)"
   type        = string
-  sensitive   = true
 }
 
 variable "client_secret" {
-  description = "Veuillez insérer votre mot de passe/secret d'application"
+  description = "Please enter your application password/secret"
   type        = string
   sensitive   = true
 }
 
 variable "tenant_id" {
-  description = "Veuillez insérer votre tenant ID Azure AD"
+  description = "Please enter your Azure AD Tenant ID"
   type        = string
 }
 
 variable "subscription_id" {
-  description = "Veuillez insérer votre subscription ID Azure"
+  description = "Please enter your Azure Subscription ID"
   type        = string
 }
 
 variable "admin_username" {
-  description = "Nom d'utilisateur de la VM Windows"
+  description = "Username for the Windows VM"
   type        = string
   default     = "arcadmin"
 }
 
 variable "admin_password" {
-  description = "Mot de passe de la VM Windows"
+  description = "Password for the Windows VM"
   type        = string
   sensitive   = true
 }
