1a8be636eb95a21dcb674f676d6a8c83ed9ca0f4
Technical And Cloud <82224047+technicalandcloud@users.noreply.github.com>
2025-06-13 17:32:56 +0200

    Update variable.tf

--- Terraform/variable.tf
+++ Terraform/variable.tf
@@ -1,43 +1,33 @@
-# Client ID (AppId) of the Azure AD application (service principal)
-variable "client_id" {
-  description = "Please enter your Client ID (AppId)"
-  type        = string
-  default     = "00000000-0000-0000-0000-000000000000" # <-- Replace with your real AppId
-}
-
-# Secret/password of the Azure AD application
-variable "client_secret" {
-  description = "Please enter your application password/secret"
-  type        = string
-  sensitive   = true
-  default     = "fake-client-secret-123" # <-- Replace with your real client secret
-}
-
-# Azure Active Directory Tenant ID
-variable "tenant_id" {
-  description = "Please enter your Azure AD Tenant ID"
-  type        = string
-  default     = "11111111-1111-1111-1111-111111111111" # <-- Replace with your real tenant ID
-}
-
-# Azure Subscription ID
-variable "subscription_id" {
-  description = "Please enter your Azure Subscription ID"
-  type        = string
-  default     = "22222222-2222-2222-2222-222222222222" # <-- Replace with your real subscription ID
-}
-
-# Admin username for the Windows VM
 variable "admin_username" {
   description = "Username for the Windows VM"
   type        = string
-  default     = "arcadmin" # <-- You can change this if needed
+  default     = "arcadmin"
 }
 
-# Admin password for the Windows VM
 variable "admin_password" {
   description = "Password for the Windows VM"
   type        = string
   sensitive   = true
-  default     = "ArcP@ssword123!" # <-- Replace with a secure password
+  default     = "ArcP@ssword123!"
+}
+
+variable "client_id" {
+  description = "Azure AD App Client ID used for onboarding to Azure Arc"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure AD App Client Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure Active Directory Tenant ID"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
 }
